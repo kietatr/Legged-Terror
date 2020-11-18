459d0f5e6710c59d816d378207c1be66ca0e825a
Kiet Tran <tran0563@umn.edu>
2020-11-18 03:17:51 -0600

    leg attached to body and ground, everything is good

---
+++ Assets/Scripts/MoveWithTerrain.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithTerrain : MonoBehaviour
+{
+    public LayerMask groundLayer;
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + Vector3.up * 5f, Vector3.down, out hit, 10f, groundLayer))
+        {
+            transform.position = hit.point;
+        }       
+    }
+}
